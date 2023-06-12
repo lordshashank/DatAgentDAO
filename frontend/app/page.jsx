8557cc3ea1f23b5e03c36bc7018c97206f32909c
lordforever <shashanktrivedi1917@gmail.com>
2023-06-13 04:36:28 +0530

    contracts completed

--- frontend/app/page.jsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-  return <h1>Hello, Next.js!</h1>;
-};
-
-export default page;
---
+++ frontend/utils/useGovernorContracts.js
@@ -0,0 +1,8 @@
+import CID from "cids";
+import { useMoralis } from "react-moralis";
+import { use, useState, useEffect } from "react";
+import contract from "../../contracts/DealClient.json";
+import { useWeb3Contract } from "react-moralis";
+
+const useGovernorContracts = () => {
+}
